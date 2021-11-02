9f2052fecdc18d6135a8d9962401f69763c846c9
Aditya Neve <aditya.neve@gmail.com>
2021-11-02 16:55:43 +0530

    pricing cards component created

---
+++ components/IndividualPricingCards.js
@@ -0,0 +1,26 @@
+function IndividualPricingCards() {
+    return `
+                <div class="standardPricingCard">
+                    <h1>Standard</h1>
+                    <h1 class="standardPrice"><sup class="rupees">₹</sup>1,499</h1>
+                    <p class="period">per month</p>
+                    <h4>Core course library, paths and skill assessments.</h4>
+                    <button class="pricingCardBtn">START WITH STANDARD</button>
+                    <p>or start a <span class="bgPink">FREE trial</span></p>
+                </div>
+                <div class="premiumPricingCard">
+                    <P class="opportunityLabel">BEST OPPORTUNITY</P>
+                    <h1>Premium</h1>
+                    <h1 class="premiumPrice"><sup class="rupees">₹</sup>2,299</h1>
+                    <p class="period">per month</p>
+                    <h4>
+                        Entire library of core and expanded courses, exams,
+                        projects and interactive courses.
+                    </h4>
+                    <button class="pricingCardBtn">START WITH PREMIUM</button>
+                    <p>or start a <span class="bgPink">FREE trial</span></p>
+                </div>
+        `;
+}
+
+export default IndividualPricingCards;
