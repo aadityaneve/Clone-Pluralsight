bffc15ec51d5c35c3b87c2c532af77e05e997449
raj-savsani <rrsavsani@gmail.com>
2021-11-02 22:28:33 +0530

    browse & sort drop-down menu completed

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,20 +1,29 @@
 let new_selected = document.getElementById("new-selected");
-  let trending = document.getElementById("trending");
-  let active_content = document.getElementById("active-content");
-  let non_active_content = document.getElementById("non-active-content");
-  new_selected.addEventListener("click", newThisMonth);
-  trending.addEventListener("click", trend);
+let trending = document.getElementById("trending");
+let active_content = document.getElementById("active-content");
+let non_active_content = document.getElementById("non-active-content");
+new_selected.addEventListener("click", newThisMonth);
+trending.addEventListener("click", trend);
 
-  function newThisMonth() {
-    new_selected.setAttribute("class", "selected");
-    trending.setAttribute("class", "non-selected");
-    active_content.setAttribute("class", "active");
-    non_active_content.setAttribute("class", "non-active");
-  }
+function newThisMonth() {
+  new_selected.setAttribute("class", "selected");
+  trending.setAttribute("class", "non-selected");
+  active_content.setAttribute("class", "active");
+  non_active_content.setAttribute("class", "non-active");
+}
 
-  function trend() {
-    trending.setAttribute("class", "selected");
-    new_selected.setAttribute("class", "non-selected");
-    active_content.setAttribute("class", "non-active");
-    non_active_content.setAttribute("class", "active");
-  }
+function trend() {
+  trending.setAttribute("class", "selected");
+  new_selected.setAttribute("class", "non-selected");
+  active_content.setAttribute("class", "non-active");
+  non_active_content.setAttribute("class", "active");
+}
+
+function dropdown() {
+    let dd_menu = document.querySelector(".dropdown-menu");
+    if(dd_menu.style.display == ""){
+        dd_menu.style.display = "block";
+    }else{
+        dd_menu.style.display = "";
+    }
+}
