24b77d201839930020d59d342f6f63779cdd4ec0
raj-savsani <rrsavsani@gmail.com>
2021-11-04 16:37:07 +0530

    skill page in making

--- scripts/courses.js
+++ scripts/courses.js
@@ -3,23 +3,23 @@ let data_div = document.getElementById("courses_data");
 
 let new_selected = document.getElementById("new-selected");
 let trending = document.getElementById("trending");
-let active_content = document.getElementById("active-content");
-let non_active_content = document.getElementById("non-active-content");
+let active_content = document.getElementById("ac");
+let non_active_content = document.getElementById("nonac");
 new_selected.addEventListener("click", newThisMonth);
 trending.addEventListener("click", trend);
 
 function newThisMonth() {
   new_selected.setAttribute("class", "selected");
   trending.setAttribute("class", "non-selected");
-  active_content.setAttribute("class", "active");
-  non_active_content.setAttribute("class", "non-active");
+  active_content.setAttribute("class", "upperactive");
+  non_active_content.setAttribute("class", "uppernonactive");
 }
 
 function trend() {
   trending.setAttribute("class", "selected");
   new_selected.setAttribute("class", "non-selected");
-  active_content.setAttribute("class", "non-active");
-  non_active_content.setAttribute("class", "active");
+  active_content.setAttribute("class", "uppernonactive");
+  non_active_content.setAttribute("class", "upperactive");
 }
 
 const courses = [
