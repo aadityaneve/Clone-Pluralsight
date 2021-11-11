965d87eafc81d60f9596c0b9555e82d1649571f9
raj-savsani <rrsavsani@gmail.com>
2021-11-11 13:35:50 +0530

    done all pages

--- scripts/courses.js
+++ scripts/courses.js
@@ -16,23 +16,6 @@ new_selected.addEventListener("click", () => {
   non_active_content.setAttribute("class", "displayNone");
 });
 
-trending.addEventListener("click", () => {
-  trending.setAttribute("class", "selected");
-  new_selected.setAttribute("class", "non-selected");
-
-  active_content.setAttribute("class", "displayNone");
-  non_active_content.setAttribute("class", "displayBlock");
-});
-
-
-setTimeout(() => {
-    // trending.setAttribute("class", "selected");
-    // new_selected.setAttribute("class", "non-selected");
-  
-    active_content.setAttribute("class", "displayBlock");
-    non_active_content.setAttribute("class", "displayNone");
-}, 1000);
-
 
 // function newThisMonth() {
   
@@ -343,6 +326,7 @@ function courseAppend(data) {
     let div = document.createElement("div");
     div.onclick = ()=>{
       localStorage.setItem("course",JSON.stringify(el));
+      location.href = "course-details.html";
     }
 
     let type = document.createElement("h4");
@@ -373,6 +357,88 @@ function courseAppend(data) {
     data_div.append(div);
   });
 }
+
 moon().then((data) => {
   courseAppend(data);
 })
+
+
+// let cards = document.getElementsByClassName("cards");
+// cards.addEventListener("click",getVal);
+
+async function getVal(v) {
+  console.log(v)
+
+  let res = await fetch('../scripts/dataSlider.json');
+  let d = await res.json();
+  let data = d.data;
+  for(let i=0;i<v;i++){
+    console.log('data[i]:', data[i])
+    localStorage.setItem("course",JSON.stringify(data[i]));
+  }
+  location.href = "course-details.html";
+}
+
+// slider js function
+
+async function slideData(){
+  let res = await fetch('../scripts/dataSlider.json');
+
+  let d = await res.json();
+
+  slide(d.data)
+}
+
+// slideData();
+
+
+let slid = document.getElementById("active-content");
+
+function slide(data){
+
+  slid.innerHTML = null;
+
+  data.forEach((el)=>{
+
+    let main_div = document.createElement("div");
+    main_div.setAttribute("id","upperCards")
+
+    let inner_div = document.createElement("div");
+    inner_div.setAttribute("id","cards");
+
+    let image = document.createElement("img");
+    image.setAttribute("class","image")
+    image.src =  el.image;
+
+    let short_div = document.createElement("div");
+    short_div.setAttribute("class","short-dis");
+
+    let h3 = document.createElement("h4");
+    h3.textContent= el.title;
+
+    let p = document.createElement("div")
+    p.textContent = el.by;
+
+    let ul = document.createElement("ul")
+    ul.setAttribute("class","cards-ul-flex")
+
+    let li1 = document.createElement("li")
+    li1.textContent = el.time;
+
+    let time_img = document.createElement("img");
+    time_img.src = "https://www.pluralsight.com/etc/clientlibs/pluralsight/main/images/course/clock.png";
+
+    let li2 = document.createElement("li")
+    li2.textContent = el.level;
+
+    let level_img = document.createElement("img");
+    level_img.src = "https://www.pluralsight.com/etc/clientlibs/pluralsight/main/images/course/level.png";
+
+    ul.append(li1,time_img,li2,level_img);
+    short_div.append(h3,p,ul);
+    inner_div.append(image,short_div);
+    main_div.append(inner_div);
+    slid.append(main_div);
+  })
+  
+}
