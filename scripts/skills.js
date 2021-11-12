24152e64adbf4e2c04d872e2576d27c94730943e
Aditya Neve <aditya.neve@gmail.com>
2021-11-12 21:55:24 +0530

    navbar and footer added

--- scripts/skills.js
+++ scripts/skills.js
@@ -1,10 +1,13 @@
 import Navbar from '../components/Navbar.js';
 import NavItems from '../components/NavItems.js'
+import Footer from '../components/Footer.js';
 
 let navItems = document.getElementById("navItems");
 let nav = document.getElementById('nav');
+let footer = document.getElementById('footer');
 navItems.innerHTML = NavItems();
 nav.innerHTML = Navbar();
+footer.innerHTML = Footer();
 
 // Click event for Collapsibles
 var collapsibles = document.getElementsByClassName("collapsible");
