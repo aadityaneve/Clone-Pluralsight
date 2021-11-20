3984a288f8a61f3f28bc382fa9ecdde923cddc6a
vaibhav kothiyal <vaibhavkkothiyal@gmail.com>
2021-11-20 20:12:01 +0530

    updated

--- scripts/subscription.js
+++
@@ -1,7 +0,0 @@
-import NavItems from "../components/NavItems.js";
-import Navbar from "../components/Navbar.js";
-
-let navItems = document.getElementById("navItems");
-let nav = document.getElementById('nav');
-navItems.innerHTML = NavItems();
-nav.innerHTML = Navbar();
