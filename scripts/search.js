680f7ee03b4f69f63ef1bda546a5ae5b48d602cf
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2021-12-19 12:35:09 +0530

    Searchbar Changes

--- scripts/search.js
+++ scripts/search.js
@@ -1,42 +1,71 @@
-let search = document.getElementById("img");
-let body = document.getElementById("mainBody");
-let parentDiv = document.getElementById("parentDiv");
+const COURSES_API = 'https://clone-pluralsight-server.herokuapp.com/courses';
 
-let searchItem = document.getElementById("searchItem");
+let search = document.getElementById('img');
+let body = document.getElementById('mainBody');
+let searchItem = document.getElementById('searchItem');
+let searchedData = document.getElementById('searchedData');
+let searchInput = document.getElementById('searchInput');
 
-searchItem.style.display = "none";
-search.addEventListener("click", () => {
-  searchItem.style.display = "flex";
-  parentDiv.style.display = "block";
+searchItem.style.display = 'none';
+searchedData.style.display = 'none';
+search.addEventListener('click', () => {
+    searchItem.style.display = 'flex';
+    searchedData.style.display = 'block';
+    document.documentElement.style.overflow = 'hidden';
 });
 
-let close = document.getElementById("p");
-close.addEventListener("click", () => {
-  searchItem.style.display = "none";
-  parentDiv.style.display = "none";
+let closeDiv = document.getElementById('p');
+closeDiv.addEventListener('click', () => {
+    searchItem.style.display = 'none';
+    searchedData.style.display = 'none';
+    document.documentElement.style.overflow = 'auto';
 });
 
-let searchInput = document.getElementById("searchInput");
-searchInput.addEventListener("keyup", () => {
-  console.log(searchInput.value);
+let searchDataTimer;
+searchInput.addEventListener('keyup', () => {
+    // console.log(searchInput.value);
+
+    if (searchDataTimer) {
+        clearInterval(searchDataTimer);
+    }
+
+    searchDataTimer = setTimeout(async () => {
+        displayData(await getCourses(COURSES_API));
+    }, 500);
 });
 
-const courses = "https://clone-pluralsight-server.herokuapp.com/courses";
-
-const getCourses = async (API) => {
-  try {
-    let response = await fetch(API);
-    let res = response.json();
-    return res;
-  } catch (e) {
-    console.log("Error: ", e);
-  }
+const getCourses = async (COURSES_API) => {
+    try {
+        let response = await fetch(COURSES_API);
+        let res = response.json();
+        return res;
+    } catch (e) {
+        console.log('Error: ', e);
+    }
 };
 
-skills();
-let allCoursesData;
-async function skills() {
-  let allCoursesFinalData = await getCourses(courses);
-  allCoursesData = allCoursesFinalData.course;
-  courseAppend(allCoursesData);
-}
+const displayData = ({ course }) => {
+    searchedData.innerHTML = null;
+    let coursesDiv = document.createElement('div');
+    coursesDiv.setAttribute('id', 'coursesDataDiv');
+
+    let searchedString = searchInput.value.trim().toLowerCase();
+
+    course = course.filter((element) => {
+        return element.title.toLowerCase().includes(searchedString);
+    });
+
+    course.forEach((element) => {
+        let title = document.createElement('p');
+        title.setAttribute('style', 'cursor: pointer;');
+        title.innerText = element.title;
+
+        title.onclick = () => {
+            localStorage.setItem('course', JSON.stringify(element));
+            location.href = 'course-details.html';
+        };
+
+        coursesDiv.append(title);
+    });
+    searchedData.append(coursesDiv);
+};
